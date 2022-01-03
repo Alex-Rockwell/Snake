a095287aa0a21987f44dcce07f80b754bb40e0f1
Alex-Rockwell <vin-ag@list.ru>
2022-01-03 13:10:16 +0400

    N2

--- src/game.js
+++ src/game.js
@@ -1,9 +1,15 @@
 import {update as updateSnake, draw as drawSnake , SNAKE_SPEED} from './snake.js'
-const gameBoard = document.querySelector('#game-board')
+import {update as updateFood, draw as drawFood} from './food.js'
 
 let lastRenderTime = 0
+const gameBoard = document.querySelector('#game-board')
+let gameOver = false
 
 function main(currentTime) {
+  if (gameOver) {
+    return alert('you lose')
+  }
+
   let secondsSinceLastRender = (currentTime - lastRenderTime) / 1000
   window.requestAnimationFrame(main)
   if (secondsSinceLastRender < 1 / SNAKE_SPEED) return
@@ -19,9 +25,16 @@ window.requestAnimationFrame(main)
 
 function update() {
   updateSnake()
+  updateFood()
+  checkDeth()
 }
 
 function draw() {
   gameBoard.innerHTML = ''
   drawSnake(gameBoard)
+  drawFood(gameBoard)
+}
+
+function checkDeath() {
+  gameOver = outsideGrid(getSnakeHead()) || snakeIntersection()
 }
--- src/snake.js
+++ src/snake.js
@@ -1,22 +1,22 @@
 import { getInputDirection } from "./input.js"
-export const SNAKE_SPEED = 1
+export const SNAKE_SPEED = 3
 
 const snakeBody = [
   {x: 11, y: 11},
 ]
+let newSegments = 0
 
 export function update() {
   const inputDirection = getInputDirection()
   for (let i = snakeBody.length - 2; i >= 0; i--) {
     snakeBody[i + 1] = {...snakeBody[i]}
   }
-
   snakeBody[0].x += inputDirection.x
   snakeBody[0].y += inputDirection.y
+  addSegments()
 }
 
 export function draw(gameBoard) {
-  console.log('draw snake')
   snakeBody.forEach(segment => {
     const snakeElement = document.createElement('div')
     snakeElement.style.gridRowStart = segment.y
@@ -24,5 +24,26 @@ export function draw(gameBoard) {
     snakeElement.classList.add('snake')
     gameBoard.appendChild(snakeElement)
   })
+}
+
+export function expandSnake(amount) {
+  newSegments += amount
+}
+
+export function onSnake(position) {
+  return snakeBody.some(segment => {
+    return equalPositions(segment, position)
+  })
+}
+
+function equalPositions(pos1, pos2) {
+  return (pos1.x === pos2.x && pos1.y === pos2.y)
+}
+
+function addSegments() {
+  for (let i=0; i < newSegments; i++) {
+    snakeBody.push({...snakeBody[snakeBody.length - 1]}) 
+  }
+  newSegments = 0
 }
 
